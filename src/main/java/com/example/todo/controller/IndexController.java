231daba350cf1ee458eb559b39013b31b7a22515
ita-jp <28795978+ita-jp@users.noreply.github.com>
2023-08-08 22:56:27 +0900

    テンプレートエンジン Thymeleaf を使おう

--- src/main/java/com/example/todo/controller/IndexController.java
+++ src/main/java/com/example/todo/controller/IndexController.java
@@ -2,7 +2,6 @@ package com.example.todo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller("/")
 public class IndexController {
@@ -10,8 +9,7 @@ public class IndexController {
     // http:localhost:8080/ -> "Hello, world!"
     // GET / -> "Hello, world!"
     @GetMapping
-    @ResponseBody
     public String index() {
-        return "Hello, world!";
+        return "index";
     }
 }
