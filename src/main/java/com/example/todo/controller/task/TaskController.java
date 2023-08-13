6d2d7bb49494f0ee0ec3173302e05df50d89c311
ita-jp <28795978+ita-jp@users.noreply.github.com>
2023-08-13 22:25:16 +0900

    タスク作成ページを表示しよう

--- src/main/java/com/example/todo/controller/task/TaskController.java
+++ src/main/java/com/example/todo/controller/task/TaskController.java
@@ -30,4 +30,9 @@ public class TaskController {
         model.addAttribute("task", TaskDTO.toDTO(taskEntity));
         return "tasks/detail";
     }
+    
+    @GetMapping("/tasks/creationForm")
+    public String showCreationForm() {
+        return "tasks/form";
+    }
 }
