f6d1521f8c0be3f8a16c0ef59611d62adb029361
ita-jp <28795978+ita-jp@users.noreply.github.com>
2023-08-14 22:30:03 +0900

    エラー時にフォームの内容を保持したままにするようにしよう

--- src/main/java/com/example/todo/controller/task/TaskController.java
+++ src/main/java/com/example/todo/controller/task/TaskController.java
@@ -37,14 +37,18 @@ public class TaskController {
     }
 
     @GetMapping("/creationForm")
-    public String showCreationForm() {
+    public String showCreationForm(TaskForm form, Model model) {
+        if (form == null) {
+            form = new TaskForm(null, null, null);
+        }
+        model.addAttribute("taskForm", form);
         return "tasks/form";
     }
 
     @PostMapping
-    public String create(@Validated TaskForm form, BindingResult bindingResult) {
+    public String create(@Validated TaskForm form, BindingResult bindingResult, Model model) {
         if (bindingResult.hasErrors()) {
-            return "tasks/form";
+            return showCreationForm(form, model);
         }
         taskService.create(form.toEntity());
         return "redirect:/tasks";
