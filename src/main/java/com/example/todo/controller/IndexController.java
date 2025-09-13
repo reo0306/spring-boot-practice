66a606ee1acd72b24c5a6dcd0e8fa6e0b0fd3d9a
reo0306 <reo19840306@gmail.com>
2025-09-14 08:20:11 +0900

    first commit

--- src/main/java/com/example/todo/controller/IndexController.java
+++ src/main/java/com/example/todo/controller/IndexController.java
@@ -4,12 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller("/")
-public class IndexController {
+public class indexController {
 
-    // http:localhost:8080/ -> "Hello, world!"
-    // GET / -> "Hello, world!"
-    @GetMapping
-    public String index() {
-        return "index";
-    }
+   @GetMapping
+   public String index() {
+       return "index";
+   }
 }
