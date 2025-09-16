e32ff4d609060c853504520d94ee36e4a0245e89
reo0306 <reo19840306@gmail.com>
2025-09-16 17:01:29 +0900

    add detail

--- src/main/java/com/example/todo/controller/task/TaskController.java
+++ src/main/java/com/example/todo/controller/task/TaskController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @RequiredArgsConstructor
@@ -21,5 +22,13 @@ public class TaskController {
 
         model.addAttribute("taskList", taskList);
         return "tasks/list";
+    }
+
+    @GetMapping("/tasks/{id}")
+    public String showDetail(@PathVariable("id") long taskId, Model model) {
+        var taskEntity = taskService.findById(taskId)
+                .orElseThrow(() -> new IllegalArgumentException("Task not found: id = " + taskId));
+        model.addAttribute("task", TaskDTO.toDTO(taskEntity));
+        return "tasks/detail";
     }
 }
