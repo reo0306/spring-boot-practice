7e4a73e4f9ddde3900e89d325401baee00665f75
reo0306 <reo19840306@gmail.com>
2025-09-27 07:01:06 +0900

    add delete process

--- src/main/java/com/example/todo/controller/task/TaskController.java
+++ src/main/java/com/example/todo/controller/task/TaskController.java
@@ -76,4 +76,12 @@ public class TaskController {
         taskService.update(entity);
         return "redirect:/tasks/{id}";
     }
+
+    @DeleteMapping("{id}")
+    public String delete(
+            @PathVariable("id") long id
+    ) {
+        taskService.delete(id);
+        return "redirect:/tasks";
+    }
 }
--- src/main/java/com/example/todo/repository/task/TaskRepository.java
+++ src/main/java/com/example/todo/repository/task/TaskRepository.java
@@ -30,4 +30,7 @@ public interface TaskRepository {
         id = #{task.id}
     """)
     void update(@Param("task") TaskEntity entity);
+
+    @Delete("DELETE FROM tasks WHERE id = #{taskId}")
+    void delete(@Param("taskId") long id);
 }
